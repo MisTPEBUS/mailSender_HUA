761df0bd403373badff63df614813eeb82081e1c
vue2023W2 <lobinda123@gmail.com>
2024-06-14 11:09:33 +0800

    update

--- services/nodeMailer.js
+++ services/nodeMailer.js
@@ -25,7 +25,7 @@ const sendMail = (email) => {
   mailOptions.html =`<!DOCTYPE html>
   <html xmlns:st1 xmlns:o>
   <head>
-      <title>首都客運-加入會員通知函</title>
+      <title>首都客運-會員EMAIL通知</title>
       <meta name="vs_targetSchema" content="http://schemas.microsoft.com/intellisense/ie5">
       <meta http-equiv="Content-Type" content="text/html; charset=big5">
       <style>
@@ -85,13 +85,14 @@ const sendMail = (email) => {
               本通知函為系統自動寄出，請勿直接回覆
           </div>
           <div class="content">
-              您好！<br><br>
-              謝謝您申請成為首都客運會員，當您收到本驗證信函後，<br><br>
-              請進行信箱驗證以正式啟用您的會員帳戶：<br><br>
-              ．聯絡信箱：${email}<br><br>
-              ．若您無法按下驗證連結，請直接複製以下網址貼到流覽器網址列：<br><br>
+              親愛的顧客您好！<br><br>
+              感謝您成為首都客運會員。由於您長時間未登入或操作，<br><br>
+              請您在下次購票時透過下方連結或是到官方網站進行重新會員驗證：<br><br>
+              ．會員信箱：${email}<br><br>
+              ．重新會員驗證網址列：<br><br>
               <a href='https://www.capitalbusgroup.com.tw/CTBUS_PORTAL/Services/CTP0601API_Q03.aspx?DATA=qyVp38OwkJBkcoPAr4s6O4R4w91E/+/V41xR6qLqCcx1MHKctwk/XQ=='>請點我進行登入驗證</a><br><br>
               如欲更新您的個人資料、密碼或查詢訂單資料，請於驗證後至【會員專區】修改及查詢。<br><br>
+              首都客運關心您，祝您旅途愉快！
           </div>
           <!-- <div class="footer">
               <img src="https://imgs.utiki.com.tw/Data/CTBUS/Mail/Template/images/logo.gif" width="280px" alt="首都客運 Logo">
